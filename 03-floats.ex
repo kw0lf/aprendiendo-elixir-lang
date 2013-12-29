f24a1ffdf69dcc7e32b8e2ccb6a7002d78f521f7
mariodevcom <hello@mariodev.com>
2013-12-29 00:34:35 -0600

    03 Floats, actualizado

--- 03-floats.ex
+++ 03-floats.ex
@@ -3,7 +3,21 @@ flotante = 1.0
 otroflotante = 2.0
 
 # Verificando si el valor es float
+##   La función is_float() devuelve true cuando el valor es del tipo
+##   IO.puts solamente se usa para darle salida a la respuesta de la función
 IO.puts is_float(flotante)
 
 # Sumando floats
-IO.puts flotante + otroflotante
+resultado =  flotante + otroflotante
+
+# Salida de los valores
+IO.puts "Resultado de la suma: #{resultado}"
+
+# Funciones para trabajar con valores de punto flotante
+##  Ceil()
+##  Redondea un flotante al entero mayor o igual
+
+techo= Float.ceil(21.2)
+
+# Salida 
+IO.puts "La función ceil(21.2) devuelve #{techo}"

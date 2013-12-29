aa505fcfb3808a5dc456a75fae40bdc57ca2c9f2
mariodevcom <hello@mariodev.com>
2013-12-28 23:14:02 -0600

    02 Enteros

---
+++ 02-integers.ex
@@ -0,0 +1,12 @@
+# Integers o enteros
+entero = 1
+otroentero = 0x1F
+
+# Verificando que los valores son enteros
+IO.puts is_integer(entero)
+IO.puts is_integer(otroentero)
+
+# Operando un suma con los dos enteros
+resultado = entero + otroentero
+
+IO.puts resultado

cd49a5dac83e02304dba62963543e598e2e6df83
Mario Martínez <hello@mariodev.com>
2015-04-23 22:15:49 -0500

    Act: 23 Abr 15

--- 06-anonymous-functions.ex
+++ 06-anonymous-functions.ex
@@ -1,24 +1,29 @@
 # Las funciones anónimas son breves unidades capaces de realizar tareas.
 # Pueden recibir uno o más valores, llamados argumentos.
-# Luego, operar sobre estos valores y devolver un resultado.
+# Luego, las funciones pueden operar sobre estos valores y devolver un
+# resultado.
 
-# Las funciones anónimas pueden declararse del mismo modo que la variables
+# Las funciones anónimas pueden declararse del mismo modo que las variables
 # que ya hemos practicado:
 
 xcien = fn a -> a * 100 end
 
 # La diferencia es el acompañamiento de las palabras clave "fn" y "end", que
-# que envuelven a la función.
+# envuelven a la función.
 
-# "a" es un espacio para una valor o argumento.
+# "a" es un espacio para una valor, al que llamaremos argumento.
 
 # "->" conduce al argumento dentro de la función.
 
-# "a * 100" multiplica por 100 al argumento.
+# "a * 100" es la expresión que indica a Elixir que multiplique por 100
+# al argumento.
 
 # Así se usa la función:
 
-xcien.(2) # Devuelve 200
+xcien.(2) # Devuelve 200.
+
+# Note el uso de un punto entre el nombre de la función y los paréntesis para
+# los argumentos.
 
 xcien.(88) # Devuelve 8800
 
@@ -39,3 +44,37 @@ mlt.(2, 100) # Devuelve 200
 # ¿Qué tal si las combinamos?
 
 mlt.(2, xcien.(2)) # Devuelve 400
+
+# Las funciones anónimas pueden declararse con expresiones más largas,
+# nombres más descriptivos y dando uso a varias líneas de escritura:
+
+area_rect = fn base, altura
+  -> base * altura / 2
+end
+
+# En un sentido opuesto, las funciones anónimas también pueden ser expresadas
+# suscintamente con el signo "&" u "Operador de Captura":
+xcien = &(&1 * 100)
+
+xcien.(20) # Devuelve 2000
+
+# El signo "&" hace las veces de la palabra clave "fn". Este mismo signo declara
+# argumentos cuando precede a un número, en el ejemplo es ""&1", es decir, el
+# primer argumento.
+
+# Una fución anónima suscinta con más de un argumento:
+mlt = &(&1 * &2)
+
+mlt.(2, 2) # Devuelve 4.
+
+# Nuestra función para calcular el área de un rectángulo con el signo "&" sería
+# algo como lo que sigue:
+
+ar = &(&1 * &2 / 2)
+
+ar.(3, 2.5) # Devuelve 3.75
+
+# Un argumento usado múltiples veces en una función:
+cubo = &(&1 * &1 * &1)
+
+cubo.(3) # Devuelve 27

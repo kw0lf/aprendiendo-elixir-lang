4714f8570fbd031ffdebbcdcfa78211336163138
Mario Martínez <hello@mariodev.com>
2015-04-24 08:39:34 -0500

    Act: 24 Abr 15

--- 02-integers.ex
+++ 02-integers.ex
@@ -1,12 +1,31 @@
-# Integers o enteros
+# Elixir reconoce dos tipos de valores numéricos. El Integer o entero es uno de
+# ellos.
+
 entero = 1
-otroentero = 0x1F
 
-# Verificando que los valores son enteros
-IO.puts is_integer(entero)
-IO.puts is_integer(otroentero)
+# Elixir entiende valores en otras notaciones: binarias, octales y
+# hexadecimales:
 
-# Operando un suma con los dos enteros
+entero_bin = 0b0111 # Equivale a 7
+
+entero_octa = 0o654 # Equivale a 428
+
+entero_hexa = 0x1F # Equivale a 31
+
+# Elixir tiene la función "is_integer" para verificar si una valor es de tipo
+# entero:
+
+is_integer(entero) # Devuelve "true"
+
+is_integer(0x1F) # Devuelve "true"
+
+is_integer("Soy un texto") # Devuelve "false"
+
+is_integer(1.02) # Devuelve "false", el valor es numérico, pero no es entero.
+
+# Operando un suma con los dos enteros y empatando el resultado con una
+# variable:
+
 resultado = entero + otroentero
 
 # Estos valores se pueden interpolar en un mensaje de salida:
